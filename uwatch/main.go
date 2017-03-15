af95e1a436908adda1f845185526bf9b736cdc98
Victor Gaydov <victor@enise.org>
2017-03-15 20:57:33 +0300

    Fix signal handling, add logging

--- uwatch/main.go
+++ uwatch/main.go
@@ -6,33 +6,55 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 )
 
-func die(f string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s[%v]: %s\n", os.Args[0], os.Getpid(), f), args...)
-	os.Exit(1)
-}
-
-func killall(pid int, sig syscall.Signal) {
-	signal.Ignore(sig)
-	for _, p := range []int{pid, -os.Getpid()} {
-		if err := syscall.Kill(p, sig); err != nil && err != syscall.ESRCH {
-			die("kill(%v, %v): %v", p, sig, err)
-		}
-	}
-}
-
 const (
-	procType   = "PROC_TYPE"
-	parentProc = "1"
+	envVar = "UWATCH_CHILD"
+)
+
+var (
+	verbose bool
 )
 
 func main() {
-	var deathTimeout time.Duration
-	flag.DurationVar(&deathTimeout, "t", time.Millisecond*200, "death timeout")
+	var (
+		pollInterval time.Duration
+		deathTimeout time.Duration
+	)
+
+	flag.DurationVar(&pollInterval, "p", time.Millisecond*100, "poll interval")
+	flag.DurationVar(&deathTimeout, "t", time.Millisecond*1000, "death timeout")
+	flag.BoolVar(&verbose, "v", false, "verbose mode")
 	flag.Parse()
+
+	if flag.NArg() == 0 {
+		die("invalid arguments: missing command")
+	}
+
+	var child *exec.Cmd
+	if _, ok := os.LookupEnv(envVar); !ok {
+		if err := os.Setenv(envVar, ""); err != nil {
+			die("setenv: %v", err)
+		}
+		child = exec.Command(os.Args[0], os.Args[1:]...)
+		deathTimeout = 0
+	} else {
+		if err := os.Unsetenv(envVar); err != nil {
+			die("unsetenv: %v", err)
+		}
+		if err := syscall.Setpgid(os.Getpid(), os.Getpid()); err != nil {
+			die("setpgid: %v", err)
+		}
+		child = exec.Command(flag.Args()[0], flag.Args()[1:]...)
+		pollInterval = 0
+	}
+
+	child.Stdin = os.Stdin
+	child.Stdout = os.Stdout
+	child.Stderr = os.Stderr
 
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs,
@@ -40,41 +62,13 @@ func main() {
 		syscall.SIGINT,
 		syscall.SIGTERM)
 
-	if flag.NArg() == 0 {
-		die("command was not specified")
-	}
-
-	var child *exec.Cmd
-	if os.Getenv(procType) == parentProc {
-		// it's a child process
-		if err := syscall.Setpgid(os.Getpid(), os.Getpid()); err != nil {
-			die("setpgid: %v", err)
-		}
-
-		child = exec.Command(flag.Args()[0], flag.Args()[1:]...)
-	} else {
-		// it's a parent process
-		child = exec.Command(os.Args[0], os.Args[1:]...)
-		if err := os.Setenv(procType, parentProc); err != nil {
-			die("setenv: %v", err)
-		}
-	}
-
-	defer func() {
-		if typ := os.Getenv(procType); typ == parentProc {
-			if err := os.Unsetenv(procType); err != nil {
-				die("unsetenv: %v", err)
-			}
-		}
-	}()
-
-	child.Stdin = os.Stdin
-	child.Stdout = os.Stdout
-	child.Stderr = os.Stderr
+	msginfo("exec: %s", strings.Join(child.Args, " "))
 
 	if err := child.Start(); err != nil {
-		die("start: %v", err)
+		die("exec: %v", err)
 	}
+
+	msginfo("exec: child pid is %v", child.Process.Pid)
 
 	done := make(chan error)
 	go func() {
@@ -84,33 +78,75 @@ func main() {
 	os.Stdin.Close()
 	os.Stdout.Close()
 
-	ticker := time.NewTicker(deathTimeout)
-	waitdeath := false
+	var (
+		pollTick <-chan time.Time
+		hardKill <-chan time.Time
+	)
+
+	if pollInterval > 0 {
+		pollTick = time.NewTicker(pollInterval).C
+	}
+
+	killAll := func(sig syscall.Signal) {
+		signal.Ignore(sig)
+		for _, pid := range []int{child.Process.Pid, -os.Getpid()} {
+			msginfo("sending %v to pid %v", strsignal(sig), pid)
+			if err := syscall.Kill(pid, sig); err != nil && err != syscall.ESRCH {
+				die("kill(%v, %v): %v", pid, strsignal(sig), err)
+			}
+		}
+	}
+
+	softKillAll := func(sig syscall.Signal) {
+		killAll(syscall.SIGHUP)
+		if hardKill == nil && deathTimeout > 0 {
+			hardKill = time.After(deathTimeout)
+		}
+	}
 
 	for {
 		select {
 		case err := <-done:
-			killall(child.Process.Pid, syscall.SIGTERM)
+			msginfo("received SIGCHLD")
+			killAll(syscall.SIGTERM)
 			if err == nil {
-				os.Exit(0)
+				exit(0)
 			} else {
-				os.Exit(1)
+				exit(1)
 			}
 
 		case sig := <-sigs:
-			killall(child.Process.Pid, sig.(syscall.Signal))
-			waitdeath = true
+			msginfo("received %v", strsignal(sig.(syscall.Signal)))
+			softKillAll(sig.(syscall.Signal))
 
-		case <-ticker.C:
-			if waitdeath {
-				killall(child.Process.Pid, syscall.SIGKILL)
-				os.Exit(1)
-			} else {
-				if os.Getppid() == 1 {
-					killall(child.Process.Pid, syscall.SIGHUP)
-					waitdeath = true
-				}
+		case <-pollTick:
+			if os.Getppid() == 1 {
+				softKillAll(syscall.SIGHUP)
 			}
+
+		case <-hardKill:
+			killAll(syscall.SIGKILL)
+			exit(1)
 		}
 	}
+}
+
+func die(f string, args ...interface{}) {
+	msg(f, args...)
+	exit(1)
+}
+
+func exit(code int) {
+	msginfo("existing with code %v", code)
+	os.Exit(code)
+}
+
+func msginfo(f string, args ...interface{}) {
+	if verbose {
+		msg(f, args...)
+	}
+}
+
+func msg(f string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s[%v]: %s\n", os.Args[0], os.Getpid(), f), args...)
 }
---
+++ uwatch/strsignal.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"syscall"
+)
+
+var signals = []struct {
+	code syscall.Signal
+	name string
+}{
+	{syscall.SIGABRT, "SIGABRT"},
+	{syscall.SIGALRM, "SIGALRM"},
+	{syscall.SIGBUS, "SIGBUS"},
+	{syscall.SIGCHLD, "SIGCHLD"},
+	{syscall.SIGCLD, "SIGCLD"},
+	{syscall.SIGCONT, "SIGCONT"},
+	{syscall.SIGFPE, "SIGFPE"},
+	{syscall.SIGHUP, "SIGHUP"},
+	{syscall.SIGILL, "SIGILL"},
+	{syscall.SIGINT, "SIGINT"},
+	{syscall.SIGIO, "SIGIO"},
+	{syscall.SIGIOT, "SIGIOT"},
+	{syscall.SIGKILL, "SIGKILL"},
+	{syscall.SIGPIPE, "SIGPIPE"},
+	{syscall.SIGPOLL, "SIGPOLL"},
+	{syscall.SIGPROF, "SIGPROF"},
+	{syscall.SIGPWR, "SIGPWR"},
+	{syscall.SIGQUIT, "SIGQUIT"},
+	{syscall.SIGSEGV, "SIGSEGV"},
+	{syscall.SIGSTKFLT, "SIGSTKFLT"},
+	{syscall.SIGSTOP, "SIGSTOP"},
+	{syscall.SIGSYS, "SIGSYS"},
+	{syscall.SIGTERM, "SIGTERM"},
+	{syscall.SIGTRAP, "SIGTRAP"},
+	{syscall.SIGTSTP, "SIGTSTP"},
+	{syscall.SIGTTIN, "SIGTTIN"},
+	{syscall.SIGTTOU, "SIGTTOU"},
+	{syscall.SIGURG, "SIGURG"},
+	{syscall.SIGUSR1, "SIGUSR1"},
+	{syscall.SIGUSR2, "SIGUSR2"},
+	{syscall.SIGVTALRM, "SIGVTALRM"},
+	{syscall.SIGWINCH, "SIGWINCH"},
+	{syscall.SIGXCPU, "SIGXCPU"},
+	{syscall.SIGXFSZ, "SIGXFSZ"},
+	{syscall.SIGUNUSED, "SIGUNUSED"},
+}
+
+func strsignal(sig syscall.Signal) string {
+	for _, s := range signals {
+		if sig == s.code {
+			return s.name
+		}
+	}
+	return "UNKNOWN"
+}
